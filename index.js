30fbd622a29e1e39f56ecbf520f9f0e108b042bd
HannahLas <129312946+HannahLas@users.noreply.github.com>
2024-02-12 09:55:44 +0000

    classes commpleted

--- index.js
+++ index.js
@@ -9,7 +9,3 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
-
-
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
-
